eaa9f9fad2454ba666ec30bc47563cbb73ef3f58
pankajsain7 <techypankaj@gmail.com>
2026-07-12 23:52:02 +0200

    fix(notification): resolve checkstyle build violation for unused import

--- services/notification-service/src/main/java/pse/trippy/notificationservice/service/EmailService.java
+++ services/notification-service/src/main/java/pse/trippy/notificationservice/service/EmailService.java
@@ -2,7 +2,6 @@ package pse.trippy.notificationservice.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
